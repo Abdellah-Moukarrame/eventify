bd9bbfee10e6cf204ebf4123114c120b218621e9
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-11-06 20:58:45 +0100

    ajouter logique de switchement entre les section aussi l etat des buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+let events = [];
+let archive = [];
+
+let btns = document.querySelectorAll(".sidebar__btn");
+let screens = document.querySelectorAll(".screen");
+
+function screenswitcher(indix) {
+  screens.forEach((section) => {
+    const datascreen = indix.dataset.screen;
+    section.classList.remove("is-visible");
+
+    if (section.dataset.screen === datascreen) {
+      section.classList.add("is-visible");
+    }
+  });
+  btns.forEach((btn) => {
+    btn.classList.remove("is-active");
+    const databutton=indix.dataset.screen;
+    if (btn.dataset.screen===databutton) {
+      btn.classList.add("is-active");
+    }
+  });
+}
